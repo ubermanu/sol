02a91a827fb575d1a88e6c81cc074aecc2c7d54e
ubermanu <manu@paca.happy-dev.fr>
2022-10-20 22:22:00 +0200

    add in memory storage adapter

---
+++ src/storage/memory.ts
@@ -0,0 +1,16 @@
+import { StorageInterface } from '../storage'
+
+const data = {}
+
+const storage: StorageInterface = {
+    read: async (resource: string) => data[resource] ?? null,
+    exists: async (resource: string) => resource in data,
+    write: async (resource: string, data: string) => {
+        data[resource] = data
+    },
+    delete: async (resource: string) => {
+        delete data[resource]
+    }
+}
+
+export default storage
